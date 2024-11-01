47fe1b6ab59fe9f6c517f40a80f8997f2a5e12d6
Jonathan Fuller <jonathan.fuller@blss.com.au>
2024-11-01 14:46:33 +0800

    first commit

---
+++ src/test/java/com/larosa/librarysystem/LibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larosa.librarysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
